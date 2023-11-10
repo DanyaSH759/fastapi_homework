f0dfe37ec82d8f694e4caeed94d0d2257d9bd873
DanyaSH <97675611+DanyaSH759@users.noreply.github.com>
2023-11-10 22:06:25 +0300

    off autorun

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def dog_change(id, name = '', kind = ''):
 
 
 # автоматически запуск api
-ngrok_tunnel = ngrok.connect(8000)
-print('Public URL:', ngrok_tunnel.public_url)
-nest_asyncio.apply()
-uvicorn.run(app, port=8000)
+#ngrok_tunnel = ngrok.connect(8000)
+#print('Public URL:', ngrok_tunnel.public_url)
+#nest_asyncio.apply()
+#uvicorn.run(app, port=8000)
